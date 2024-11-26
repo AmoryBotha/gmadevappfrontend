50ec39679dc6b196daa7c0fa67eb788d463afb1f
Amory <abotha181@gmail.com>
2024-11-26 11:01:34 +0200

    other pages nav2

--- src/components/OwnerLanding.jsx
+++ src/components/OwnerLanding.jsx
@@ -1,68 +1,77 @@
-const webID = localStorage.getItem('webIdStore');
-const conId = localStorage.getItem('conIdStore');
-const mail = localStorage.getItem('usernameStore');
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function OwnerLanding1(){
-    return <div>
-        <h1>OWNER LANDING PAGE</h1>
-        <h1>ACCOUNTS</h1>
-        <p>Under Construction...</p>
+const webID = localStorage.getItem('webIdStore') || 'N/A';
+const conId = localStorage.getItem('conIdStore') || 'N/A';
+const mail = localStorage.getItem('usernameStore') || 'N/A';
 
+function OwnerLanding1() {
+    return (
         <div>
-            <h4>Accounts Linked To Your Account</h4>
-            <p>ABC</p>
+            <h1>OWNER LANDING PAGE</h1>
+            <h1>ACCOUNTS</h1>
+            <p>Under Construction...</p>
+
             <div>
-            <div className="inline-container">
-            <div className="inline-box">
-            Owner Account Number
-            <p>Test Acc</p>
-            </div>
-            <div className="inline-box">
-            Reg/ID No
-            <p>645395938</p>
-            </div>
-            <div className="inline-box">
-            Phone Number
-            <p>09876789980</p>
-            </div>
-            <div className="inline-box">
-            Email
-            <p>{mail}</p>
-            </div>
-            <div className="inline-box">
-            <li class='form-button'><Link to ="/account">Modify</Link> </li>
-            </div>
-            </div>
-            
-            
-            <div className="inline-container">
-            <div className="inline-box">
-            Levy Account
-            <p>GMA001</p>
-            </div>
-            <div className="inline-box">
-            Building
-            <p>Test BC</p>
-            </div>
-            <div className="inline-box">
-            Door Number
-            <p>8</p>
-            </div>
-            <div className="inline-box">
-            Current Balance
-            <p>R 9000,09</p>
-            </div>
-            <div className="inline-box">
-            <li class='form-button'><Link to ="/statement">Download Statement</Link> </li>
-            </div>
-            <div className="inline-box">
-            <li class='form-button'><Link to ="/levyAccounts">Edit</Link> </li>
-            </div>
-            </div>
+                <h4>Accounts Linked To Your Account</h4>
+                <p>ABC</p>
+                <div>
+                    <div className="inline-container">
+                        <div className="inline-box">
+                            <strong>Owner Account Number</strong>
+                            <p>Test Acc</p>
+                        </div>
+                        <div className="inline-box">
+                            <strong>Reg/ID No</strong>
+                            <p>645395938</p>
+                        </div>
+                        <div className="inline-box">
+                            <strong>Phone Number</strong>
+                            <p>09876789980</p>
+                        </div>
+                        <div className="inline-box">
+                            <strong>Email</strong>
+                            <p>{mail}</p>
+                        </div>
+                        <div className="inline-box">
+                            <li className="form-button">
+                                <Link to="/account">Modify</Link>
+                            </li>
+                        </div>
+                    </div>
+
+                    <div className="inline-container">
+                        <div className="inline-box">
+                            <strong>Levy Account</strong>
+                            <p>GMA001</p>
+                        </div>
+                        <div className="inline-box">
+                            <strong>Building</strong>
+                            <p>Test BC</p>
+                        </div>
+                        <div className="inline-box">
+                            <strong>Door Number</strong>
+                            <p>8</p>
+                        </div>
+                        <div className="inline-box">
+                            <strong>Current Balance</strong>
+                            <p>R 9000,09</p>
+                        </div>
+                        <div className="inline-box">
+                            <li className="form-button">
+                                <Link to="/statement">Download Statement</Link>
+                            </li>
+                        </div>
+                        <div className="inline-box">
+                            <li className="form-button">
+                                <Link to="/levyAccounts">Edit</Link>
+                            </li>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-
+    );
 }
 
-export default OwnerLanding1
+export default OwnerLanding1;
