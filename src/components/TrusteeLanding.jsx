0ef61514cb7d3249e410aef102d5d2c77ba84d1e
Amory <abotha181@gmail.com>
2025-01-09 12:25:33 +0200

    Approval View Page

--- src/components/TrusteeLanding.jsx
+++ src/components/TrusteeLanding.jsx
@@ -55,7 +55,7 @@ function TrusteeLanding1() {
 
   const handleApprovalClick = (approval) => {
     if (view !== "active") {
-      navigate("/approvals", { state: { approval } });
+      navigate("/approval", { state: { approval } });
     }
   };
 
